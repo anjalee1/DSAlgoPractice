573a1d7579ac899c78eefc3174175f8e9f764f3a
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2021-08-09 21:29:35 +0530

    Update Print-Decreasing.py

--- recursion and backtracking/Print-Decreasing.py
+++ recursion and backtracking/Print-Decreasing.py
@@ -1,8 +1,9 @@
 
-def print_decrease(n):
-  if n==0:
-    return
-  print(n)
-  print_decrease(n-1)
+def Decreasing(n):
+    if n==0:
+        return
+    print(n)
+    Decreasing(n-1)
+    
 n= int(input())
-print_decrease(n)
+Decreasing(n)
