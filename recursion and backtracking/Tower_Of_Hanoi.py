8ab6a18251848e430433423df44d43f517f425bf
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2021-09-01 01:08:45 +0530

    Update Tower_Of_Hanoi.py

--- recursion and backtracking/Tower_Of_Hanoi.py
+++ recursion and backtracking/Tower_Of_Hanoi.py
@@ -1,1 +1,12 @@
+def toh(n,t1id,t2id,t3id):
+    if n==0:
+        return
+    toh(n-1,t1id,t3id,t2id)
+    print(f"{n}[{t1id} -> {t2id}]")
+    toh(n-1,t3id,t2id,t1id)
+n=int(input())
+t1id=int(input())
+t2id=int(input())
+t3id=int(input())
+toh(n,t1id,t2id,t3id)
 
