5c9e0377ed8cfba03ee051a3e24d5c1c0dff565c
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2021-09-23 21:53:09 +0530

    Update first_index.py

--- recursion and backtracking/Recursion in Arrays/first_index.py
+++ recursion and backtracking/Recursion in Arrays/first_index.py
@@ -1,1 +1,17 @@
 
+import sys
+sys.setrecursionlimit(10**6)
+def firstIndex(arr,idx,x):
+    if idx==len(arr):
+        return -1
+    if arr[idx]==x:
+        return idx
+    else:
+        fiisa=firstIndex(arr,idx+1,x)
+        return fiisa
+n=int(input())
+arr=[]
+for  i in range(0,n):
+    arr.append(int(input()))
+x=int(input())
+print(firstIndex(arr,0,x))
