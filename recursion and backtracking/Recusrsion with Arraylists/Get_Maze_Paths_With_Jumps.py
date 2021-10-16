0cf356960dcff7496380ef09b96b747f2ba86a6e
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2021-10-16 23:03:36 +0530

    Update Get_Maze_Paths_With_Jumps.py

--- recursion and backtracking/Recusrsion with Arraylists/Get_Maze_Paths_With_Jumps.py
+++ recursion and backtracking/Recusrsion with Arraylists/Get_Maze_Paths_With_Jumps.py
@@ -2,18 +2,21 @@ def getMazePaths(sr,sc,dr,dc):
     if sr==dr and sc==dc:
         lst=[""]
         return lst
-    hpath=[]
-    vpath=[]
-    if sc<dc:
-        hpath=getMazePaths(sr, sc+1,dr, dc)
-    if sr<dr:
-        vpath=getMazePaths(sr+1,sc,dr,dc)
     paths =[]
+    for s in range(1,dc-sc+1):
+        hpaths=getMazePaths(sr,sc+s,dr,dc)
+        for x in hpaths:
+            paths.append("h"+str(s)+str(x))
     
-    for x in hpath:
-        paths.append("h"+str(x))
-    for x in vpath:
-        paths.append("v"+str(x))
+    for s in range(1,dr-sr+1):
+        vpaths=getMazePaths(sr+s,sc,dr,dc)
+        for x in vpaths:
+            paths.append("v"+str(s)+str(x))
+            
+    for s in range(1,(dr-sr+1 and dc-sc+1)):
+        dpaths=getMazePaths(sr+s,sc+s,dr,dc)
+        for x in dpaths:
+            paths.append("d"+str(s)+str(x))
     return paths
 n=int(input())
 m=int(input())
