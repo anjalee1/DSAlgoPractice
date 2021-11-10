23c30831d87e0cc286b36f2a3f0c13942e07e6bd
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2021-11-11 00:20:41 +0530

    Update Fibonacci.py

--- Dynamic Programming/Fibonacci.py
+++ Dynamic Programming/Fibonacci.py
@@ -1,1 +1,17 @@
 
+def fun(n, dp):
+ dp[0]=0
+ dp[1]=1
+ for i in range(2,n+1):
+    dp[i]=dp[i-1]+dp[i-2]
+    
+ return dp[n] 
+
+  
+def main():
+    n = int(input())
+    dp = [0] * (n + 1)
+    print(fun(n, dp));
+
+if __name__ == "__main__":
+  main()
