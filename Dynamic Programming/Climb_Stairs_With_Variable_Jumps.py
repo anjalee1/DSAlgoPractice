18a329c3483c3b93064c46caef3f37e33e3c85d1
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2021-11-11 23:48:09 +0530

    Update Climb_Stairs_With_Variable_Jumps.py

--- Dynamic Programming/Climb_Stairs_With_Variable_Jumps.py
+++ Dynamic Programming/Climb_Stairs_With_Variable_Jumps.py
@@ -1,1 +1,25 @@
 
+def fun(n, arr, dp):
+
+    dp[n]=1
+    for i in range(n-1,-1,-1):
+      for j in range(1,arr[i]+1):
+        if i+j<len(dp):
+            dp[i] += dp[i+j]
+    return dp[0]
+
+
+# driver code
+def main():
+  n = int(input())
+  arr = []
+
+  for i in range(0, n) :
+        arr.append(int(input()))
+
+  dp = [0] * (n + 1)
+
+  print(fun(n, arr, dp))
+
+if __name__ == "__main__":
+         main()
