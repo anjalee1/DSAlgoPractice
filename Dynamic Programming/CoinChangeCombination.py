a923d4e09b928fbf3b5099a0f89fc0c41f724f29
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2021-12-19 01:18:56 +0530

    Update CoinChangeCombination.py

--- Dynamic Programming/CoinChangeCombination.py
+++ Dynamic Programming/CoinChangeCombination.py
@@ -1,1 +1,13 @@
 
+n=int(input())
+arr=[]
+for i in range(n):
+    arr.append(int(input()))
+amt=int(input())
+
+dp=[0]*(amt+1)
+dp[0]=1
+for i in range(len(arr)):
+    for j in range(arr[i],len(dp)):
+        dp[j] +=dp[j-arr[i]]
+print(dp[amt])
