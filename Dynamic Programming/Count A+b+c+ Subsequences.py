de251e3f18541a124bfbc948f3e612fd7689efbb
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-01-04 23:00:48 +0530

    Update Count A+b+c+ Subsequences.py

--- Dynamic Programming/Count A+b+c+ Subsequences.py
+++ Dynamic Programming/Count A+b+c+ Subsequences.py
@@ -1,1 +1,14 @@
 
+string=input()
+a=0
+ab=0
+abc=0
+for i in range(len(string)):
+    cr=string[i]
+    if cr=="a":
+        a=(2*a)+1
+    elif cr=="b":
+        ab=(2*ab)+a
+    elif cr=="c":
+        abc=(2*abc)+ab
+print(abc)
