51431da63f87110268bdb5380c9cf2dfe8610588
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-01-28 01:42:33 +0530

    Update PartitionIntoSubsets.py

--- Dynamic Programming/PartitionIntoSubsets.py
+++ Dynamic Programming/PartitionIntoSubsets.py
@@ -1,1 +1,25 @@
 
+n=int(input())
+k=int(input())
+if n==0 or k==0 or n<k:
+    print(0)
+    
+dp=[]
+for t in range(k+1):
+    a=[]
+    for p in range(n+1):
+        a.append(0)
+    dp.append(a)
+
+
+
+for t in range(1,k+1):
+    for p in range(1,n+1):
+        if p<t:
+            dp[t][p]=0
+        elif p==t:
+            dp[t][p]=1
+        else:
+            dp[t][p]=dp[t-1][p-1]+dp[t][p-1]*t
+    
+print(dp[t][p])
