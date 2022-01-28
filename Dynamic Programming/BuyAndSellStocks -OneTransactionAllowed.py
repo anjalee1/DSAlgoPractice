f640141f55286dc9d08c839d253b3a54ed16b724
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-01-29 00:45:33 +0530

    Update BuyAndSellStocks -OneTransactionAllowed.py

--- Dynamic Programming/BuyAndSellStocks -OneTransactionAllowed.py
+++ Dynamic Programming/BuyAndSellStocks -OneTransactionAllowed.py
@@ -1,1 +1,16 @@
 
+import sys
+n=int(input())
+prices=[]
+for i in range(n):
+    prices.append(int(input()))
+lsf=sys.maxsize
+pist=0
+op=0
+for i in range(len(prices)):
+    if prices[i]<lsf:
+        lsf=prices[i]
+    pist=prices[i]-lsf
+    if pist>op:
+        op=pist
+print(op)
