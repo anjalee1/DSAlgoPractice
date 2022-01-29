8b9893406e83e08ab06eefed566628465a468e77
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-01-29 17:28:03 +0530

    Update BuyAndSellStocks-InfiniteTransactionsAllowed.py

--- Dynamic Programming/BuyAndSellStocks-InfiniteTransactionsAllowed.py
+++ Dynamic Programming/BuyAndSellStocks-InfiniteTransactionsAllowed.py
@@ -1,1 +1,17 @@
 
+import sys
+n=int(input())
+prices=[]
+for i in range(n):
+    prices.append(int(input()))
+bd=0
+sd=0
+profit=0
+for i in range(1,len(prices)):
+    if prices[i]>prices[i-1]:
+        sd +=1
+    else:
+        profit += prices[sd]-prices[bd]
+        bd=sd=i
+profit += prices[sd]-prices[bd]
+print(profit)
