a64486abb4ca788ccb4608734b12460d92e11136
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-03-12 12:58:29 +0530

    Update CelebrityProblem.py

--- Stack/CelebrityProblem.py
+++ Stack/CelebrityProblem.py
@@ -1,1 +1,35 @@
+def findCelebrity(arr):
+    st = []
+    for i in range(len(arr)):
+        st.append(i)
+        
+    while len(st) > 1:
+        a = st.pop()
+        b = st.pop()
 
+        if int(arr[a][b]) == 1:
+            st.append(b)
+        else:
+            st.append(a)
+
+    # Possible Celebrity
+    pc = st.pop()     
+    
+    for i in range(len(arr)):
+        if i != pc:
+            if int(arr[i][pc]) == 0 or int(arr[pc][i]) == 1:
+                print("none")
+                return
+
+    print(pc,end="\n")
+
+
+
+n = int(input())
+arr = []
+
+for i in range(n):          
+    row = input()
+    arr.append(row)
+
+findCelebrity(arr)
