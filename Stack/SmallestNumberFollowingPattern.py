43b7c9e94c7e8b686f22e5e4648a52eb17f1e7d0
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-03-28 22:39:24 +0530

    Update SmallestNumberFollowingPattern.py

--- Stack/SmallestNumberFollowingPattern.py
+++ Stack/SmallestNumberFollowingPattern.py
@@ -1,1 +1,28 @@
 
+from collections import deque
+def main():
+    inp = str(input())
+    stack=deque()
+
+    # write your code here
+    num=1
+    for char in inp:
+        if char=='d':
+            stack.append(num)
+            num=num+1
+
+        else:
+            stack.append(num)
+            num=num+1
+
+            while len(stack)>0:
+                print(stack.pop(),end='')
+                
+    stack.append(num)
+
+    while len(stack)>0:
+            print(stack.pop(),end='')
+    
+        
+        
+main()
