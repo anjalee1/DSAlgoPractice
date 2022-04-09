caa4ee50c7eff17dcb066278e04eb45023ab50da
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-04-10 01:39:21 +0530

    Create NormalStack.py

---
+++ Stack/NormalStack.py
@@ -0,0 +1,84 @@
+class CustomStack:
+       
+    def __init__(self , cap):
+        self.cap = cap
+        self.tos = -1
+        self.arr = []
+        self.arr = [0] * cap
+       
+    def size(self):
+        return self.tos+1
+        
+       
+    def push(self , data):
+        # write your code here
+        if self.tos==self.cap-1:
+            print("Stack overflow")
+            return
+        else:
+            self.tos=self.tos+1
+            self.arr[self.tos]=data
+   
+       
+    def top(self):
+        # write ur code here
+       
+       if self.tos==-1:
+          print("Stack underflow")
+          return -1
+       else:
+           return self.arr[self.tos]
+   
+    def pop(self):
+        # write ur code here
+        if self.tos==-1:
+          print("Stack underflow")
+          return -1
+        else:
+            rv=self.arr[self.tos]
+            self.tos=self.tos-1
+            return rv
+       
+    def display(self):
+        # write ur code here
+        i=self.tos
+        while i>=0:
+            print(self.arr[i].strip(),end=' ')
+            i=i-1
+        print()
+        
+        
+        
+
+
+
+def main():
+   
+    n = int(input());
+   
+    inpStr = str(input()).split(" ")
+    st = CustomStack(n)
+   
+    while inpStr[0] != "quit":
+        if inpStr[0].strip() == "push":
+            val = inpStr[1]
+            st.push(val)
+        elif inpStr[0].strip() == "pop":
+            val = st.pop()
+            if val != -1:
+                print(val)
+        elif inpStr[0].strip() == "top":
+            val = st.top()
+            if val != -1:
+                print(val)
+        elif inpStr[0].strip() == "size":
+            print(st.size())
+        elif inpStr[0].strip() == "display":
+            st.display()
+           
+        inpStr = str(input()).split(" ")
+           
+   
+   
+   
+main()
