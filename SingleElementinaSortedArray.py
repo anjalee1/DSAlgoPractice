c24fda589128b33c6e93e34fc6e595c216ad27f0
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-05-30 00:21:36 +0530

    Create  SingleElementinaSortedArray.py

---
+++ SingleElementinaSortedArray.py
@@ -0,0 +1,28 @@
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        n=len(nums)
+        if n==1 or (nums[0]!=nums[1]):
+            return nums[0]
+        if nums[n-1]!=nums[n-2]:
+             return nums[n-1]
+        low=0
+        high=n-1
+        while low<=high:
+            mid=low+(high-low)//2
+            if nums[mid] != nums[mid-1] and nums[mid]!=nums[mid+1]:
+                return nums[mid]
+                
+            elif nums[mid]==nums[mid-1]:
+                lo=mid-low+1
+                if lo%2==0:
+                    low=mid+1
+                else:
+                    high=mid-2
+            elif nums[mid]==nums[mid+1]:
+                hi=high-mid+1
+                if hi%2==0:
+                    high=mid-1
+                else:
+                    low=mid+2
+        
+                
