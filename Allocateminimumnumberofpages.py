d1f9d0cf8f2eb1e00e335c4ac1bc26e2b0e70d5d
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-06-03 00:50:32 +0530

    Create Allocateminimumnumberofpages.py

---
+++ Allocateminimumnumberofpages.py
@@ -0,0 +1,48 @@
+class Solution:
+    
+    #Function to find minimum number of pages.
+    
+    def isPossible(self,arr,mid,noOfStudents):
+        st=1
+        sum=0
+        for x in arr:
+            sum += x
+            if sum>mid:
+                st+=1
+                sum=x
+        return st<=noOfStudents
+                
+            
+        
+    def findPages(self,A, N, M):
+        #code here
+        lo=max(A)
+        hi=sum(A)
+        while lo<=hi:
+            mid=(hi-lo)//2 + lo
+            if self.isPossible(A,mid,M)==True:
+                ans=mid
+                hi=mid-1
+            else:
+                lo=mid+1
+        return ans
+
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+#contributed by RavinderSinghPB
+if __name__=='__main__':
+    t=int(input())
+    for _ in range(t):
+        
+        n=int(input())
+        
+        arr=[int(x) for x in input().strip().split()]
+        m=int(input())
+        
+        ob=Solution()
+        
+        print(ob.findPages(arr,n,m))
+# } Driver Code Ends
