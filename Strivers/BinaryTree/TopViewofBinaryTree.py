fdf348d2b129412b634f76aa70ce14900958f8f4
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-06-10 02:01:57 +0530

    Update TopViewofBinaryTree.py

--- Strivers/BinaryTree/TopViewofBinaryTree.py
+++ Strivers/BinaryTree/TopViewofBinaryTree.py
@@ -1,1 +1,134 @@
 
+#User function Template for python3
+
+# Tree Node
+# class Node:
+#     def __init__(self, val):
+#         self.right = None
+#         self.data = val
+#         self.left = None
+
+class Solution:
+    
+    #Function to return a list of nodes visible from the top view 
+    #from left to right in Binary Tree.
+    
+    def Width(self,root,hl,ans):
+        if root is None:
+            return
+        ans[0] = min(hl, ans[0])
+        ans[1] = max(hl, ans[1])
+        
+        self.Width(root.left, hl - 1, ans)
+        self.Width(root.right, hl + 1, ans)
+
+    
+    def topView(self,root):
+        if root is None:
+            return []
+        que=[]
+        minMax=[0,0]
+        self.Width(root, 0, minMax)
+        length = minMax[1] - minMax[0] + 1;
+        ans = [None]*length
+        que.append(tuple((root, abs(minMax[0]))))
+        while len(que) != 0:
+            size = len(que)
+            while (size >0):
+                rp = que.pop(0)
+                size=size-1
+                if ans[rp[1]] == None:
+                    ans[rp[1]]=rp[0].data
+
+                if rp[0].left is not None:
+                    que.append(tuple((rp[0].left, rp[1] - 1)))
+
+                if rp[0].right is not None:
+                     que.append(tuple((rp[0].right, rp[1] + 1)))
+        return ans
+
+            
+         
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+from collections import deque
+# Tree Node
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+
+# Function to Build Tree   
+def buildTree(s):
+    #Corner Case
+    if(len(s)==0 or s[0]=="N"):           
+        return None
+        
+    # Creating list of strings from input 
+    # string after spliting by space
+    ip=list(map(str,s.split()))
+    
+    # Create the root of the tree
+    root=Node(int(ip[0]))                     
+    size=0
+    q=deque()
+    
+    # Push the root to the queue
+    q.append(root)                            
+    size=size+1 
+    
+    # Starting from the second element
+    i=1                                       
+    while(size>0 and i<len(ip)):
+        # Get and remove the front of the queue
+        currNode=q[0]
+        q.popleft()
+        size=size-1
+        
+        # Get the current node's value from the string
+        currVal=ip[i]
+        
+        # If the left child is not null
+        if(currVal!="N"):
+            
+            # Create the left child for the current node
+            currNode.left=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.left)
+            size=size+1
+        # For the right child
+        i=i+1
+        if(i>=len(ip)):
+            break
+        currVal=ip[i]
+        
+        # If the right child is not null
+        if(currVal!="N"):
+            
+            # Create the right child for the current node
+            currNode.right=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.right)
+            size=size+1
+        i=i+1
+    return root
+    
+    
+if __name__=="__main__":
+    t=int(input())
+    for _ in range(0,t):
+        s=input()
+        root=buildTree(s)
+        ob= Solution()
+        
+        res = ob.topView(root)
+        for i in res:
+            print(i,end=" ")
+        print()
+# } Driver Code Ends
