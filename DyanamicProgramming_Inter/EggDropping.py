8602fc32b9a73c8ac30448d682c80f65466af67a
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-07-25 21:14:23 +0530

    Update EggDropping.py

--- DyanamicProgramming_Inter/EggDropping.py
+++ DyanamicProgramming_Inter/EggDropping.py
@@ -1,1 +1,35 @@
 
+import sys
+
+def eggDrop(eggs,floors):
+    dp=[[0 for col in range (floors+1)]for row in range(eggs+1)]
+    for e in range(1,eggs+1):
+        for f in range(1,floors+1):
+            if e==1:
+                dp[e][f]=f
+
+            elif f==1:
+                dp[e][f]=1
+            
+            else:
+                minm=sys.maxsize
+                mrf=f-1
+                prf=0
+                while mrf>=0:
+                   val1=dp[e][mrf]
+                   val2=dp[e-1][prf]
+                   maxv=max(val1,val2)
+                   minm=min(maxv,minm)
+                   mrf=mrf-1
+                   prf=prf+1
+                dp[e][f]=minm+1
+    return dp[eggs][floors]
+
+
+def main():
+    eggs= int(input())
+    floors=int(input())
+    print(eggDrop(eggs,floors))
+
+if __name__ == "__main__":
+    main()
