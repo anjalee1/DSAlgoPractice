9821d092911f24b68dc61f2c55f69823db0c4c13
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-07-26 20:40:15 +0530

    Update WordBreak.py

--- DyanamicProgramming_Inter/WordBreak.py
+++ DyanamicProgramming_Inter/WordBreak.py
@@ -1,1 +1,25 @@
 
+def WordBreak(sentence,dictionary):
+    dp=[0]*len(sentence)
+    for i in range(0,len(dp)):
+        for j in range(0,i+1):
+            if sentence[j:i+1] in dictionary:
+                if j>0:
+                    dp[i] += dp[j-1]
+                else:
+                    dp[i] += 1
+
+
+    return dp[len(sentence)-1]>0
+
+def main():
+    n=int(input())
+    dictionary= input()
+    sentence=input()
+    if WordBreak(sentence,dictionary):
+        print("true")
+    else:
+        print("false")
+
+if __name__ == "__main__":
+    main()
