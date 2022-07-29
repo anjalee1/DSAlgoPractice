4dc2ab973c2d8706fcf5ff3a0b61ac7b890500e3
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-07-30 02:15:35 +0530

    Update ceil.py

--- BinarySearchTree/ceil.py
+++ BinarySearchTree/ceil.py
@@ -1,1 +1,24 @@
 
+'''
+    Following is the TreeNode class structure
+
+    class TreeNode:
+        def __init__(self, data):
+            self.data = data
+            self.left = None
+            self.right = None
+'''
+
+def findCeil(root, x):
+    ans= - 1
+    if root is None:
+        return -1
+    while root:
+        if root.data==x:
+            return root.data
+        elif root.data<x:
+            root=root.right
+        else:
+            ans=root.data
+            root=root.left
+    return ans
