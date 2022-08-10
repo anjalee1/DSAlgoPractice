1f4c98a4e31e594ccd2cebd54e81fb1f47c243fe
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-08-10 17:18:42 +0530

    Update ReverseWordsinaString.py

--- String/ReverseWordsinaString.py
+++ String/ReverseWordsinaString.py
@@ -1,1 +1,22 @@
 
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        end = len(s) - 1
+        ans = ""    
+        left=0
+        while left <len(s) and s[left]==' ':
+            left=left+1
+        right=len(s)-1
+        while right >0 and s[right]==' ':
+            right=right-1
+        temp=''
+        for i in range(left,right+1):
+            if s[i]==' ' and s[i-1] !=' ':
+                ans=" "+temp+ans
+                temp=''
+            elif s[i]!=' ':
+                temp=temp+s[i]
+            if i==right:
+                ans=temp+ans  
+                            
+        return ans
