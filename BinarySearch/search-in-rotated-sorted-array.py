d92dc6b0bd739de00f0374b1e2597191b5afbe7a
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-08-13 17:55:45 +0530

    Update search-in-rotated-sorted-array.py

--- BinarySearch/search-in-rotated-sorted-array.py
+++ BinarySearch/search-in-rotated-sorted-array.py
@@ -18,3 +18,6 @@ class Solution:
                 hi=mid-1
         return -1
             
+#tc=o(log(n))
+#solutionvideo = https://www.youtube.com/watch?v=1uu3g_uu8O0&t=832s
+#ide=https://leetcode.com/problems/search-in-rotated-sorted-array/
