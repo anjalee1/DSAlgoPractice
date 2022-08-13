c76352e3d0b656476d9a232379d2ea2b12b5a00e
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-08-13 17:42:46 +0530

    Update SingleElementinaSortedArray.py

--- BinarySearch/SingleElementinaSortedArray.py
+++ BinarySearch/SingleElementinaSortedArray.py
@@ -25,6 +25,7 @@ class Solution:
                 else:
                     low=mid+2
         
-                
+#tc=o(logn)
+#sc=o(1)
 #ide :https://leetcode.com/problems/single-element-in-a-sorted-array/
 #solutionvideo: https://www.youtube.com/watch?v=WFNa5o-dHGo&t=1464s
