656f92bf56e13dcc43bdeb9adf2c25ac26b96909
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-09-13 00:09:26 +0530

    Create ImplementQueueUsingStack.java

---
+++ Queue/ImplementQueueUsingStack.java
@@ -0,0 +1,28 @@
+class MyQueue {
+
+    Stack<Integer> input = new Stack();
+    Stack<Integer> output = new Stack();
+    
+    public void push(int x) {
+        input.push(x);
+    }
+
+    public void pop() {
+        peek();
+        output.pop();
+    }
+
+    public int peek() {
+        if (output.empty())
+            while (!input.empty())
+                output.push(input.pop());
+        return output.peek();
+    }
+
+    public boolean empty() {
+        return input.empty() && output.empty();
+    }
+}
+
+//tc:push=>o(1)
+  //pop=>o(n)
