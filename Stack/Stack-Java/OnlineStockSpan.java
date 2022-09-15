add7aa9bf9a055abb5aebf2c2045c5551b274561
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-09-15 16:33:18 +0530

    Update OnlineStockSpan.java

--- Stack/Stack-Java/OnlineStockSpan.java
+++ Stack/Stack-Java/OnlineStockSpan.java
@@ -1,1 +1,18 @@
 
+class StockSpanner {
+    Stack<int []> st;
+    public StockSpanner() {
+        st=new Stack<>();
+    }
+    
+    public int next(int price) {
+     int span =1; 
+      while(st.size()>0 && st.peek()[0]<=price){
+          span+=st.pop()[1];
+      }
+        st.push(new int[]{price,span});
+      return span;
+    }
+}
+
+//leetcode link : https://leetcode.com/problems/online-stock-span/submissions/
