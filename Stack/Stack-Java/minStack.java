8edd42c899557786203be6bd00e5cf1776a2ab35
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-09-18 17:39:51 +0530

    Update minStack.java

--- Stack/Stack-Java/minStack.java
+++ Stack/Stack-Java/minStack.java
@@ -1,1 +1,34 @@
 
+class MinStack {
+    Stack<Integer> as;
+    Stack<Integer> min;
+    
+    public MinStack() {
+      as = new  Stack<Integer>();
+      min = new Stack<Integer>();
+    }
+    
+    public void push(int val) {
+         as.push(val);
+        if(min.empty()||val<=.peek()){
+            min.push(val);
+        }  
+    }
+    
+    public void pop() {
+        int val =as.peek();
+        as.pop();
+        if(val==min.peek()){
+            min.pop();
+        }
+    }
+    
+    public int top() {
+       return as.peek() ;
+    }
+    
+    public int getMin() {
+        return min.peek();
+        
+    }
+}
