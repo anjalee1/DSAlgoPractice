55350e4e59be895c702bc863c5a4a1f7afe37be2
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-09-19 01:17:21 +0530

    Update LRU_Cache.java

--- Stack/Stack-Java/LRU_Cache.java
+++ Stack/Stack-Java/LRU_Cache.java
@@ -1,1 +1,91 @@
 
+class LRUCache {
+    class Node{
+        int key,val;
+        Node prev,next;
+    }
+    
+     public void addNode(Node node){
+        Node nbr=head.next;
+         
+        node.next=nbr;
+        node.prev=head;
+         
+        nbr.prev=node;
+        head.next=node;
+
+    }
+    
+    public void removeNode(Node node){
+            Node prev=node.prev;
+            Node forw = node.next;
+            
+           prev.next=forw;
+           forw.prev=prev;
+           node.prev=node.next=null;
+                
+     }
+    
+    public void moveToFront(Node node){
+        removeNode(node);
+        addNode(node);
+
+    }
+
+    HashMap<Integer, Node> cache;
+    Node head,tail;
+    int cap;
+   
+    public LRUCache(int capacity) {
+        cache=new HashMap<>();
+        cap=capacity;
+        head=new Node();
+        tail=new Node();
+        
+        head.next=tail;
+        tail.prev=head;
+        
+    }
+    
+    public int get(int key) {
+        Node node=cache.get(key);
+        if(node==null){
+            return -1;
+        }
+        else{
+            int vl=node.val;
+            moveToFront(node);
+            return vl;
+            
+        }
+    }
+    
+    public void put(int key, int value) {
+        Node node=cache.get(key);
+        if(node==null){
+            Node newNode=new Node();
+            newNode.key=key;
+            newNode.val=value;
+            if(cache.size()==cap){
+                Node LRU_Node=tail.prev;
+                cache.remove(LRU_Node.key);
+                removeNode(LRU_Node);
+                
+            }
+            
+            cache.put(key,newNode);
+            addNode(newNode);      
+            
+        }
+        else{
+            node.val=value;
+            moveToFront(node);
+        }
+        
+    }
+}
+
+/**
+ * tc of put and get in hashmap is o(1)
+ * tc of add and remove in double ll is o(1)
+ */
