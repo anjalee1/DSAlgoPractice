c928533c5388dd170b09e876aa5a949792c1f85b
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-09-19 20:05:57 +0530

    Update CelebrityProblem.java

--- Stack/Stack-Java/CelebrityProblem.java
+++ Stack/Stack-Java/CelebrityProblem.java
@@ -1,1 +1,38 @@
 
+import java.util.* ;
+import java.io.*; 
+
+public class Solution {
+	public static int findCelebrity(int n) {
+        Stack<Integer> st= new Stack<Integer>();
+        for(int i=0;i<n;i++){
+            st.push(i);
+        } 
+        while(st.size()>1){
+            int i=st.pop();
+            int j=st.pop();
+            if(Runner.knows(i,j)){
+                st.push(j);
+            }
+            else{
+                st.push(i);
+            }
+        } 
+        if(st.empty())
+            return -1;
+        int pot=st.pop();
+        for(int i=0;i<n;i++){
+            if(i!=pot){
+                if(Runner.knows(i,pot)==false || Runner.knows(pot,i)==true){
+                    return -1;
+                }
+            }
+         }
+        return pot; 
+    }
+}
+
+//solution video:nsoluhttps://www.youtube.com/watch?v=CiiXBvrX-5A&t=7s
+//ide: https://www.codingninjas.com/codestudio/problems/the-celebrity-problem_982769?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website&leftPanelTab=0
+//tc:o(N)
+//sc:o(N)
