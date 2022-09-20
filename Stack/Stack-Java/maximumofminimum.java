d48e89ac8e22bca78d78bb8ad071fef0e64f27fc
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-09-20 15:59:01 +0530

    Update maximumofminimum.java

--- Stack/Stack-Java/maximumofminimum.java
+++ Stack/Stack-Java/maximumofminimum.java
@@ -1,1 +1,6 @@
 
+https://www.geeksforgeeks.org/find-the-maximum-of-minimums-for-every-window-size-in-a-given-array/
+https://www.youtube.com/watch?v=muvjEHpeVig
+
+//for a given window size max will be their either inside the  window found through nse left and nse right  
+//or the max ansfor larger window size than this.
