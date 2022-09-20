8ddc1aab42eeb0ad3cb601d367b91e3400b0dcc1
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-09-20 12:13:07 +0530

    Update MiddleofLL.java

--- LinkedList/LinkedList_Java/MiddleofLL.java
+++ LinkedList/LinkedList_Java/MiddleofLL.java
@@ -1,1 +1,25 @@
 
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        
+        if(head==null || head.next==null){
+            return head;
+        }
+        
+        ListNode slow=head;
+        ListNode fast=head;
+        
+        while(fast.next!=null && fast.next.next != null){
+              slow =slow.next;
+              fast=fast.next.next;
+        }
+        
+        if(fast.next == null){
+            return slow;
+        }
+        else{
+            return slow.next;
+        }
+        
+    }
+}
