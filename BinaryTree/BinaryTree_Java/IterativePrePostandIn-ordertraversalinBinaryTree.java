d77dcc77d0ae97c00c8094432280fe0004259482
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-09-30 15:18:07 +0530

    Create IterativePrePostandIn-ordertraversalinBinaryTree.java

---
+++ BinaryTree/BinaryTree_Java/IterativePrePostandIn-ordertraversalinBinaryTree.java
@@ -0,0 +1,8 @@
+//Time Complexity: O(N)
+//Reason: We are visiting every node thrice therefore time complexity will be O(3*N), which can be assumed as linear time complexity
+//Space Complexity: O(N)
+//Reason: We are using three lists and a stack to store the nodes. The time complexity will be about O(4*N), which can be assumed as linear time complexity.
+
+//ide: https://www.codingninjas.com/codestudio/problems/981269?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website
+
+
