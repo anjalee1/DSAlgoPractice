5bbf3d8d96e0080952d3c612f63a74c9d6d2c969
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-09-30 13:40:34 +0530

    Update DeletionInABinaryTree.java

--- BinaryTree/BinaryTree_Java/DeletionInABinaryTree.java
+++ BinaryTree/BinaryTree_Java/DeletionInABinaryTree.java
@@ -1,1 +1,54 @@
 
+def deleteDeepest(root,d_node):
+    q = []
+    q.append(root)
+    while(len(q)):
+        temp = q.pop(0)
+        if temp is d_node:
+            temp = None
+            return
+        if temp.right:
+            if temp.right is d_node:
+                temp.right = None
+                return
+            else:
+                q.append(temp.right)
+        if temp.left:
+            if temp.left is d_node:
+                temp.left = None
+                return
+            else:
+                q.append(temp.left)
+  
+                  
+ def deletion(root, key):
+    if root == None :
+        return None
+    if root.left == None and root.right == None:
+        if root.key == key :
+            return None
+        else :
+            return root
+    key_node = None
+    q = []
+    q.append(root)
+    temp = None
+    while(len(q)):
+        temp = q.pop(0)
+        if temp.data == key:
+            key_node = temp
+        if temp.left:
+            q.append(temp.left)
+        if temp.right:
+            q.append(temp.right)
+    if key_node :
+        x = temp.data
+        deleteDeepest(root,temp)
+        key_node.data = x
+    return root
+   
+   //solution:https://www.geeksforgeeks.org/deletion-binary-tree/
+ //Time complexity: O(n) where n is no number of nodes
+//Auxiliary Space: O(n) size of queue
+
+//Note: We can also replace the node’s data that is to be deleted with any node whose left and right child points to NULL but we only use deepest node in order to maintain the Balance of a binary tree.
