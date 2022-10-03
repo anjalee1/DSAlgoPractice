8c1d068d821a504b9b586724032598baf2563035
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-10-03 22:04:34 +0530

    Update PostOrderTraversal.java

--- BinaryTree/BinaryTree_Java/PostOrderTraversal.java
+++ BinaryTree/BinaryTree_Java/PostOrderTraversal.java
@@ -34,3 +34,55 @@ class Solution {
         list.add(root.val);
 }
 }
+
+//Iterative approach
+//Time Complexity: O(N).
+//Reason: We are traversing N nodes and every node is visited exactly once.
+//Space Complexity: O(N+N)
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root == null) return list;
+
+        Stack<TreeNode> s1=new Stack<>();
+        Stack<TreeNode> s2=new Stack<>();
+        
+        s1.push(root);
+        
+        while(!s1.isEmpty()){
+            TreeNode el=s1.pop();
+            s2.push(el);
+            
+            if(el.left!=null){
+                 s1.push(el.left);
+            }
+            if(el.right!=null){
+                 s1.push(el.right);
+            }
+           
+        }
+        
+        while(!s2.isEmpty()){
+            list.add(s2.pop().val);
+        }
+        
+        return list;
+       
+    }
+}
