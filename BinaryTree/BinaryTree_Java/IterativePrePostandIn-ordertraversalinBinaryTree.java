6e569335880a44e5eb8731193df7fad26c658dd8
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-10-04 00:20:47 +0530

    Update IterativePrePostandIn-ordertraversalinBinaryTree.java

--- BinaryTree/BinaryTree_Java/IterativePrePostandIn-ordertraversalinBinaryTree.java
+++ BinaryTree/BinaryTree_Java/IterativePrePostandIn-ordertraversalinBinaryTree.java
@@ -6,3 +6,75 @@
 //ide: https://www.codingninjas.com/codestudio/problems/981269?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website
 
 
+import java.util.* ;
+import java.io.*; 
+/************************************************************
+
+	Following is the Binary Tree node structure:
+
+	class BinaryTreeNode<T> {
+	    T data;
+	    BinaryTreeNode<T> left;
+	    BinaryTreeNode<T> right;
+
+	    public BinaryTreeNode(T data) {
+	        this.data = data;
+	    }
+	}
+
+************************************************************/
+
+public class Solution {
+    static class Pair{
+        BinaryTreeNode<Integer> node ;
+        int state;
+        Pair(BinaryTreeNode<Integer> node, int state) {
+             this.node = node;
+             this.state = state;
+        }
+    }
+    
+    public static List<List<Integer>> getTreeTraversal(BinaryTreeNode<Integer> root) {
+       
+        List<List<Integer>> res = new ArrayList<>();
+        if(root==null){
+            return res;
+        }
+        List < Integer > pre = new ArrayList < > ();
+        List < Integer > in = new ArrayList < > ();
+        List < Integer > post = new ArrayList < > ();
+        Stack<Pair> st = new Stack<>();
+        Pair first=new Pair(root,1);
+        st.push(first);
+        while(!st.isEmpty()){
+            Pair pair= st.peek();
+            if(pair.state==1){
+                pre.add(pair.node.data);
+                pair.state++;
+                
+                if (pair.node.left != null) {
+                    st.push(new Pair(pair.node.left, 1));
+                }
+            }
+            else if(pair.state ==2){
+                 in.add(pair.node.data);
+                 pair.state++;
+                
+                if (pair.node.right!= null) {
+                    st.push(new Pair(pair.node.right, 1));
+                }
+            }
+            else{
+               post.add(pair.node.data);
+               st.pop();
+            }
+        }
+       
+        
+        res.add(in);
+        res.add(pre);
+        res.add(post);
+        
+        return res;
+    }
+}
