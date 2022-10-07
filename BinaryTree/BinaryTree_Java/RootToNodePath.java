1e2255fc33d425f0d769a8282b7c27fbb3ef95a0
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-10-07 17:53:14 +0530

    Create RootToNodePath.java

---
+++ BinaryTree/BinaryTree_Java/RootToNodePath.java
@@ -0,0 +1,7 @@
+Time Complexity: O(N)
+Reason: We are doing a simple tree traversal.
+Space Complexity: O(N)
+Reason: In the worst case (skewed tree), space complexity can be O(N).
+  
+ //ide: 
+  //sol:
