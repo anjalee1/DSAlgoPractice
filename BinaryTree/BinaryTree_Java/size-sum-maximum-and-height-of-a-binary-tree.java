54f565342f04be90e5b789f1c7b8afed05d6d5e1
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-10-07 19:06:03 +0530

    Update size-sum-maximum-and-height-of-a-binary-tree.java

--- BinaryTree/BinaryTree_Java/size-sum-maximum-and-height-of-a-binary-tree.java
+++ BinaryTree/BinaryTree_Java/size-sum-maximum-and-height-of-a-binary-tree.java
@@ -1,2 +1,8 @@
 https://nados.io/question/size-sum-maximum-and-height-of-a-binary-tree
 https://www.youtube.com/watch?v=Y7fg3QS6u6w&t=4s
+
+
+height of a binary tree
+Time Complexity: O(N) 
+Space Complexity: O(1) Extra Space + O(H) Recursion Stack space, where “H”  is the height of the binary tree.
+  https://takeuforward.org/data-structure/maximum-depth-of-a-binary-tree/
