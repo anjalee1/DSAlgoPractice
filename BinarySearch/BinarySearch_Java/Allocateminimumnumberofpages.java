d2ac6a457226c389c3ee17fc46b8648549d4d182
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-10-09 02:24:32 +0530

    Update Allocateminimumnumberofpages.java

--- BinarySearch/BinarySearch_Java/Allocateminimumnumberofpages.java
+++ BinarySearch/BinarySearch_Java/Allocateminimumnumberofpages.java
@@ -1,5 +1,5 @@
 
-//ide:https://www.codingninjas.com/codestudio/problems/ayush-gives-ninjatest_1097574?topList=striver-sde-sheet-problems&utm_source=striver&utm_medium=website&leftPanelTab=0
+//ide:https://www.codingninjas.com/codestudio/problems/allocate-books_1090540
 //sol:https://www.youtube.com/watch?v=okP-e2VpI_g
 // Time Complexity : O(NlogN)
 // Reason: Binary search takes O(log N). For every search, we are checking if an allocation is possible or not. Checking for allocation takes O(N).
@@ -9,43 +9,47 @@
 import java.util.* ;
 import java.io.*; 
 public class Solution {
-    public static long ayushGivesNinjatest(int n, int m, int[] time) {
-       int  maxTime=Integer.MIN_VALUE;;
-        long sum=0;
-        long answer =0;
-        for(int i =0;i<m;i++){
-             sum += time[i];
-             maxTime = Math.max(maxTime , time[i]);
-        }
-        long l=maxTime,r=sum;
-       while(l<=r){
-            long mid = (l + r) / 2;
-            long days = 1;
-            long currentTime = 0;
-           
-            for (int i = 0; i < m; i++) {
-                currentTime += time[i];
-                if (currentTime > mid) {
-                    days++;
-                    currentTime = time[i];
-                }
-
-            }
-           
-            if (days <= n) {
-                answer = mid;
-                r = mid - 1;
-            }
-
-            else {
-                l = mid + 1;
-            }
-
-
-       }
-        
-        return answer;
-    }
     
-   
+    
+    public static boolean isPossible(ArrayList<Integer> arr, int mid, int m) {
+    int st = 1;
+    int sum = 0;
+    for (int i = 0; i < arr.size(); i++) {
+      sum += arr.get(i);
+      if (sum > mid) {
+        sum = arr.get(i);
+        st++;
+      }
+    }
+    return st <= m;
+  }
+
+    public static int allocateBooks(ArrayList<Integer> arr, int n, int m) {
+        if (m > arr.size()) return -1;
+
+        int sum = 0;
+        int max = 0;
+        for (Integer val : arr) {
+          sum += val;
+          max = Math.max(max, val);
+        }
+
+        int lo = max;
+        int hi = sum;
+        int ans = 0;
+
+        while (lo <= hi) {
+          int mid = lo + (hi - lo) / 2;
+
+          if (isPossible(arr, mid, m) == true) {
+            ans = mid;
+            hi = mid - 1;
+          }
+          else {
+            lo = mid + 1;
+          }
+        }
+
+        return ans;
+    }
 }
