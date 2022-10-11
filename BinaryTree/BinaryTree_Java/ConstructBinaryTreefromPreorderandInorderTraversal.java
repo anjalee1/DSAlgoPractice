cfa5e80e881354b297d7ba771a9cd9e9247e760a
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-10-11 18:53:24 +0530

    Update ConstructBinaryTreefromPreorderandInorderTraversal.java

--- BinaryTree/BinaryTree_Java/ConstructBinaryTreefromPreorderandInorderTraversal.java
+++ BinaryTree/BinaryTree_Java/ConstructBinaryTreefromPreorderandInorderTraversal.java
@@ -1,1 +1,8 @@
+// Time Complexity: O(N)
+// Assumption: Hashmap returns the answer in constant time.
+// Space Complexity: O(N)
+// Reason: We are using an external hashmap of size ‘N’.
 
+//ide: https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+//sol: https://takeuforward.org/data-structure/construct-a-binary-tree-from-inorder-and-preorder-traversal/
+//https://www.youtube.com/watch?v=aZNaLrVebKQ
