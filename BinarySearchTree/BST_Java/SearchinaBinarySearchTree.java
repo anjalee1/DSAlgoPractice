eb43148a82459a2745e0b4d53d7360d3616a61d4
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-10-18 01:10:18 +0530

    Create SearchinaBinarySearchTree.java

---
+++ BinarySearchTree/BST_Java/SearchinaBinarySearchTree.java
@@ -0,0 +1,42 @@
+
+//ide: https://leetcode.com/problems/search-in-a-binary-search-tree/
+//sol: https://www.youtube.com/watch?v=Lr2oxJlnLMM
+
+//tc: o(N)
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        
+        TreeNode curr=root;
+        
+        while(curr!=null){
+            if(curr.val==val){
+                return curr;
+            }
+            else if(curr.val>val){
+                curr=curr.left;
+            }
+             else if(curr.val<val){
+                curr=curr.right;
+            }
+        }
+        
+        return null;
+        
+    }
+}
