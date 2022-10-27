63b25bd08192df6702cd5ab679314b56b978fbfe
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-10-27 11:48:42 +0530

    Update KthSmallestElementinaBST.java

--- BinarySearchTree/BST_Java/KthSmallestElementinaBST.java
+++ BinarySearchTree/BST_Java/KthSmallestElementinaBST.java
@@ -1,1 +1,39 @@
 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int count = 0;
+    int result = Integer.MIN_VALUE;
+
+    public int kthSmallest(TreeNode root, int k) {
+        traverse(root, k);
+        return result;
+    }
+
+    public void traverse(TreeNode root, int k) {
+        if(root == null) return;
+        traverse(root.left, k);
+        count ++;
+        if(count == k) result = root.val;
+        traverse(root.right, k);       
+    }
+}
+
+//ide: https://leetcode.com/problems/kth-smallest-element-in-a-bst/
+//sol: https://www.youtube.com/watch?v=9TJYWh0adfk
+
+//Time Complexity: O(min(K,N))
+//Space Complexity: O(min(K,N))
