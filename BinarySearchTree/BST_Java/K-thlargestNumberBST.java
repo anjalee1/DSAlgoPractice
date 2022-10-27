98b0b8ebdbeeca13b25be33705d7dd1d7c6d4229
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-10-27 14:18:02 +0530

    Update K-thlargestNumberBST.java

--- BinarySearchTree/BST_Java/K-thlargestNumberBST.java
+++ BinarySearchTree/BST_Java/K-thlargestNumberBST.java
@@ -1,1 +1,46 @@
 
+/*
+    Time Complexity: O(N)
+    Space Complexity: O(N)
+
+    where 'N' is the total number of nodes of the BST.
+*/
+    
+import java.util.ArrayList;
+
+public class Solution {
+	public static int revInorder(TreeNode < Integer > root, int [] visCount, int k)
+	{
+		if (root == null)
+		{
+			return -1;
+		}
+		
+		// Recurse over right subtree. 
+		int right = revInorder(root.right, visCount, k);
+	    
+	    if (right != -1)
+	    {
+	    	return right;
+	    }
+
+		visCount[0]++;
+
+		if (visCount[0] == k)
+		{
+			return root.data;
+		}
+
+		// Recurse over left subtree.
+		int left = revInorder(root.left, visCount, k);
+	    
+	    return left;
+	}
+
+
+	public static int KthLargestNumber(TreeNode<Integer> root, int k) {
+		int[] visCount = new int [1];
+
+		return revInorder(root, visCount, k);
+	}
+}
