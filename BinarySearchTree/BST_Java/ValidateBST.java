9731d7b6fefde2b8bb3c1efc14854b8f7279548f
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-11-03 00:21:50 +0530

    Update ValidateBST.java

--- BinarySearchTree/BST_Java/ValidateBST.java
+++ BinarySearchTree/BST_Java/ValidateBST.java
@@ -1,5 +1,36 @@
 //ide: https://leetcode.com/problems/validate-binary-search-tree/
+//sol: https://www.youtube.com/watch?v=f-sj7I5oXEI&t=308s
+//TC: O(N)
+//SC: O(N)
 
-//sol: https://www.youtube.com/watch?v=kMrbTnd5W9U
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root,Long.MIN_VALUE,Long.MAX_VALUE);
+    }
+    
+    public boolean isValidBST(TreeNode root,long minVal ,long maxVal) {
+        if(root==null) return true;
+        
+        if(root.val>=maxVal || root.val <=minVal) return false;
+        
+        return isValidBST(root.left,minVal,root.val) && 
+            isValidBST(root.right,root.val,maxVal);
+    }
+}
 
-//
+
