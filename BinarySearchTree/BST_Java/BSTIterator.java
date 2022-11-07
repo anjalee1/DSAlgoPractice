b92d189d3d3924469b6cb08165819cc646f01342
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-11-07 23:20:52 +0530

    Create BSTIterator.java

---
+++ BinarySearchTree/BST_Java/BSTIterator.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class BSTIterator {
+    
+    private Stack<TreeNode> stack = new  Stack<TreeNode>(); 
+    
+    public BSTIterator(TreeNode root) {
+        pushAll(root);
+    }
+    
+    public int next() {
+        TreeNode tmpNode= stack.pop();
+        pushAll(tmpNode.right);
+        return tmpNode.val;
+    }
+    
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+    
+    private void pushAll(TreeNode node){
+        while(node != null){
+            stack.push(node);
+            node=node.left;
+            
+        }
+        
+    }
+}
+
+//tc : o(1)
+
+//sc :o(H)
+
+//vid : https://www.youtube.com/watch?v=D2jMcmxU4bs
+
+//ide: https://leetcode.com/problems/binary-search-tree-iterator/
