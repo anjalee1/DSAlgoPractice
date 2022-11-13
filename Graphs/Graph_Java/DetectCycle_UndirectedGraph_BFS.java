8a9b4c341e46f04776842e71656030812a6c64b5
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-11-13 16:06:07 +0530

    Update DetectCycle_UndirectedGraph_BFS.java

--- Graphs/Graph_Java/DetectCycle_UndirectedGraph_BFS.java
+++ Graphs/Graph_Java/DetectCycle_UndirectedGraph_BFS.java
@@ -1,1 +1,96 @@
 
+//IDE: https://practice.geeksforgeeks.org/problems/detect-cycle-in-an-undirected-graph/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=detect-cycle-in-an-undirected-graph
+//SOL: https://www.youtube.com/watch?v=BPlrALf1LDU
+//https://takeuforward.org/data-structure/detect-a-cycle-in-undirected-graph-breadth-first-search/
+
+//TC: O(N+2E)+O(N)
+//SC: O(N)
+
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            for (int i = 0; i < V; i++) adj.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            boolean ans = obj.isCycle(V, adj);
+            if (ans)
+                System.out.println("1");
+            else
+                System.out.println("0");
+        }
+    }
+}
+// } Driver Code Ends
+
+class Node {
+    int node;
+    int parent;
+    public Node(int node, int parent) {
+        this.node = node;
+        this.parent = parent; 
+    }
+}
+
+
+class Solution {
+    
+    
+    static boolean checkForCycle(ArrayList<ArrayList<Integer>> adj, int s, boolean vis[])
+    {
+       Queue<Node> q =  new LinkedList<>(); //BFS
+       q.add(new Node(s, -1));
+       vis[s] =true;
+       
+       // until the queue is empty
+       while(!q.isEmpty())
+       {
+           // source node and its parent node
+           int node = q.peek().node;
+           int par = q.peek().parent;
+           q.remove(); 
+           
+           // go to all the adjacent nodes
+           for(Integer it: adj.get(node))
+           {
+               if(vis[it]==false)  
+               {
+                   q.add(new Node(it, node));
+                   vis[it] = true; 
+               }
+        
+                // if adjacent node is visited and is not its own parent node
+               else if(par != it) return true;
+           }
+       }
+       
+       return false;
+    }
+    // Function to detect cycle in an undirected graph.
+    public boolean isCycle(int V, ArrayList<ArrayList<Integer>> adj) {
+        boolean vis[] = new boolean[V];
+        for(int i=0;i<V;i++){
+            if(vis[i]==false) 
+                if(checkForCycle(adj, i,vis)) 
+                    return true;
+        }
+        return false;
+    }
+}
+
