67b93b269fba643215510efb1eed739e135a2b4c
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-11-15 16:58:01 +0530

    Create BipartiteGraph_Dfs.java

---
+++ Graphs/Graph_Java/BipartiteGraph_Dfs.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isBipartite(int[][] graph) {
+        int n = graph.length;
+        int[] colors = new int[n];			
+				
+        for (int i = 0; i < n; i++) {              //This graph might be a disconnected graph. So check each unvisited node.
+            if (colors[i] == 0 && !validColor(graph, colors, 1, i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public boolean validColor(int[][] graph, int[] colors, int color, int node) {
+        if (colors[node] != 0) {
+            return colors[node] == color;
+        }       
+        colors[node] = color;       
+        for (int next : graph[node]) {
+            if (!validColor(graph, colors, -color, next)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+
+//ide:https://leetcode.com/problems/is-graph-bipartite/
+//sol:https://www.youtube.com/watch?v=KG5YFfR0j8A&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&index=19
+//https://takeuforward.org/data-structure/bipartite-check-using-dfs-if-graph-is-bipartite/
+
+// Time Complexity: O(V + E), since in its whole, it is a DFS implementation, V – vertices; E – edges;
+// Space Complexity: O(V), because, apart from the graph, we maintain a color array.
