89790ffa81b73d038fa17c7c423f7b97f422f93f
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-11-17 16:36:37 +0530

    Create KosarajuAlgorithm.java

---
+++ Graphs/Algos_Java/KosarajuAlgorithm.java
@@ -0,0 +1,8 @@
+// Time Complexity: O(N+E), DFS+TopoSort
+// Space Complexity: O(N+E), Transposing the graph
+
+//sol:https://www.youtube.com/watch?v=V8qIqJxCioo&list=PLgUwDviBIf0rGEWe64KWas0Nryn7SCRWw&index=28
+//https://takeuforward.org/data-structure/kosarajus-algorithm-for-strongly-connected-componentsscc/
+//also check this for theory : https://www.youtube.com/watch?v=QtdE7QPsWiU&t=1634s
+
+//ide: 
