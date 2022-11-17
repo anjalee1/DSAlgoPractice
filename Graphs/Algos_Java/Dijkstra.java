858b7fb09c1edb5d0b0e231ecb8b80fbdb8b853a
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-11-17 18:13:47 +0530

    Create Dijkstra.java

---
+++ Graphs/Algos_Java/Dijkstra.java
@@ -0,0 +1,7 @@
+//ide : https://practice.geeksforgeeks.org/problems/shortest-path-in-weighted-undirected-graph/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=shortest-path-in-weighted-undirected-graph
+//sol:https://www.youtube.com/watch?v=rp1SMw7HSO8&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&index=36
+//https://takeuforward.org/data-structure/dijkstras-algorithm-shortest-distance/
+//theory : https://www.youtube.com/watch?v=sD0lLYlGCJE
+
+// Time Complexity: O((N+E)*logN). Going through N nodes and E edges and log N for priority queue
+// Space Complexity: O(N). Distance array and priority queue
