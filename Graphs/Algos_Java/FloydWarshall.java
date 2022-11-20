77d3fedbba9a2295bb5beac092fa7d2bee5c2500
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-11-20 21:29:12 +0530

    Create FloydWarshall.java

---
+++ Graphs/Algos_Java/FloydWarshall.java
@@ -0,0 +1,90 @@
+//ide:https://practice.geeksforgeeks.org/problems/implementing-floyd-warshall2042/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=implementing-floyd-warshall
+//sol:https://www.youtube.com/watch?v=PwMVNSJ5SLI&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&index=43
+//tc: o(n^3)
+//sc:o(n)
+
+
+//{ Driver Code Starts
+//Initial template for JAVA
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            int[][] matrix = new int[n][n];
+            for(int i = 0; i < n; i++){
+                String[] s = br.readLine().trim().split(" ");
+                for(int j = 0; j < n; j++)
+                    matrix[i][j]  =Integer.parseInt(s[j]);
+            }
+            Solution obj = new Solution();
+            obj.shortest_distance(matrix);
+            for(int i = 0; i < n; i++){
+                for(int j  = 0; j < n; j++){
+                    System.out.print(matrix[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function template for JAVA
+
+class Solution
+{
+    public void shortest_distance(int[][] matrix)
+    {
+        int n =matrix.length;
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                
+                if(matrix[i][j]==-1){
+                    matrix[i][j]=(int)1e9;
+                }
+                
+                if(i==j){
+                    matrix[i][j]=0;
+                }
+                
+            }
+        }
+        
+        for(int k=0;k<n;k++){
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    matrix[i][j]=Math.min(matrix[i][j],
+                    matrix[i][k]+matrix[k][j]);
+                    
+                    
+                }
+            }
+        }
+        
+        
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==(int)1e9){
+                    matrix[i][j]=-1;
+                }
+                
+            }
+        }
+    
+    }    
+        
+}
+
