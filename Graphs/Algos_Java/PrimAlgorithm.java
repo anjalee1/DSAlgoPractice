c95413e55418f42e116f49c510b74df1bae31e73
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-11-20 21:45:53 +0530

    Create PrimAlgorithm.java

---
+++ Graphs/Algos_Java/PrimAlgorithm.java
@@ -0,0 +1,6 @@
+//ide:https://practice.geeksforgeeks.org/problems/minimum-spanning-tree/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=minimum-spanning-tree
+//mst: https://www.youtube.com/watch?v=xsM8i0jVF1w&list=PLgUwDviBIf0rGEWe64KWas0Nryn7SCRWw&index=18
+
+//sol: https://www.youtube.com/watch?v=mJcZjjKzeqk&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&index=45
+//https://takeuforward.org/data-structure/minimum-spanning-tree-mst-using-prims-algo/
+//
