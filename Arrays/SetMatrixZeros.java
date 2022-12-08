3606e4bbba67bfaf49a18112479a663253124e1c
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-12-08 15:53:01 +0530

    Update SetMatrixZeros.java

--- Arrays/SetMatrixZeros.java
+++ Arrays/SetMatrixZeros.java
@@ -1,1 +1,28 @@
+//ide:https://leetcode.com/problems/set-matrix-zeroes/description/
+//sol: https://www.youtube.com/watch?v=M65xBewcqcI&list=PLgUwDviBIf0rPG3Ictpu74YWBQ1CaBkm2&index=14
 
+// Time Complexity: O(2*(N*M)), as we are traversing two times in a matrix,
+// Space Complexity: O(1).
+
+
+class Solution {
+    public void setZeroes(int[][] matrix) {
+      int col0 = 1, rows = matrix.length, cols = matrix[0].length;
+
+        for (int i = 0; i < rows; i++) {
+            if (matrix[i][0] == 0) col0 = 0;
+            for (int j = 1; j < cols; j++)
+                if (matrix[i][j] == 0)
+                    matrix[i][0] = matrix[0][j] = 0;
+        }
+
+        for (int i = rows - 1; i >= 0; i--) {
+            for (int j = cols - 1; j >= 1; j--)
+                if (matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
+            if (col0 == 0) matrix[i][0] = 0;
+        }
+
+        
+    }
+}
