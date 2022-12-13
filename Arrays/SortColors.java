4917a75ce9a32cffd7701b3b30693400dcef6f4a
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-12-13 22:56:47 +0530

    Create SortColors.java

---
+++ Arrays/SortColors.java
@@ -0,0 +1,36 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int lo = 0; 
+        int hi = nums.length - 1; 
+        int mid = 0; 
+        int temp; 
+        while (mid <= hi) { 
+            switch (nums[mid]) { 
+                case 0: { 
+                    temp = nums[lo]; 
+                    nums[lo] = nums[mid]; 
+                    nums[mid] = temp; 
+                    lo++; 
+                    mid++; 
+                    break; 
+                } 
+                case 1: 
+                    mid++; 
+                    break; 
+                case 2: { 
+                    temp = nums[mid]; 
+                    nums[mid] = nums[hi]; 
+                    nums[hi] = temp; 
+                    hi--; 
+                    break; 
+                } 
+            } 
+        }
+    }
+}
+
+// Time Complexity: O(N)
+// Space Complexity: O(1)
+
+  //ide: https://leetcode.com/problems/sort-colors/description/
+  //sol: https://www.youtube.com/watch?v=oaVa-9wmpns&list=PLgUwDviBIf0rPG3Ictpu74YWBQ1CaBkm2&index=4
