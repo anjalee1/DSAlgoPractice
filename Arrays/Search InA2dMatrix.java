9b84fabb39fc7f327c3991c1a4f456e5a4db1db0
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-12-15 17:03:33 +0530

    Create Search InA2dMatrix.java

---
+++ Arrays/Search InA2dMatrix.java
@@ -0,0 +1,39 @@
+//gfg variation =>https://www.geeksforgeeks.org/search-in-row-wise-and-column-wise-sorted-matrix/
+
+
+//ide:https://leetcode.com/problems/search-a-2d-matrix/description/
+//sol:https://www.youtube.com/watch?v=kQ1mJlwW-c0
+//https://takeuforward.org/data-structure/count-inversions-in-an-array/
+
+//Time complexity: O(log(m*n))
+//Space complexity: O(1)
+
+
+
+
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+
+        int lo = 0;
+        if(matrix.length == 0) return false;
+        int n = matrix.length; 
+        int m = matrix[0].length; 
+        int hi = (n * m) - 1;
+        
+        while(lo <= hi) {
+            int mid = (lo + (hi - lo) / 2);
+            if(matrix[mid/m][mid % m] == target) {
+                return true;
+            }
+            if(matrix[mid/m][mid % m] < target) {
+                lo = mid + 1;
+            }
+            else {
+                hi = mid - 1;
+            }
+        }
+        return false;
+        
+    }
+}
