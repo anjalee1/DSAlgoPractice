8ea9e4856a6f61f9390796ac6d0a651fe5cd636a
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-12-16 17:50:22 +0530

    Update FourSum.java

--- Arrays/FourSum.java
+++ Arrays/FourSum.java
@@ -6,3 +6,67 @@
 // Reason: There are 2 nested loops and the front pointer as well as the right pointer (Third nested loop)
 
 // Space Complexity: O(1), (Generally the space complexity that is used to return the answer is ignored)
+
+
+
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        
+        if (nums == null || nums.length == 0)
+            return res;
+        
+        int n = nums.length;
+    
+        Arrays.sort(nums); 
+    
+        for (int i = 0; i < n; i++) {
+        
+            long target_3 = target - nums[i];
+        
+            for (int j = i + 1; j < n; j++) {
+            
+                long target_2 = target_3 - nums[j];
+            
+                int front = j + 1;
+                int back = n - 1;
+            
+                while(front < back) {
+                
+                    int two_sum = nums[front] + nums[back];
+                
+                    if (two_sum < target_2) front++;
+                
+                    else if (two_sum > target_2) back--;
+                
+                    else {
+                    
+                        List<Integer> quad = new ArrayList<>(); 
+                        quad.add(nums[i]);
+                        quad.add(nums[j]);
+                        quad.add(nums[front]);
+                        quad.add(nums[back]);
+                        res.add(quad);
+                    
+                        // Processing the duplicates of number 3
+                        while (front < back && nums[front] == quad.get(2)) ++front;
+                    
+                        // Processing the duplicates of number 4
+                        while (front < back && nums[back] == quad.get(3)) --back;
+                
+                    }
+                }
+                
+                // Processing the duplicates of number 2
+                while(j + 1 < n && nums[j + 1] == nums[j]) ++j;
+            }
+        
+            // Processing the duplicates of number 1
+            while (i + 1 < n && nums[i + 1] == nums[i]) ++i;
+        
+        }
+        
+    
+        return res;
+    }
+}
