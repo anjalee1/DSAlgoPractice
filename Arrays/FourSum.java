6376b7c8b9abae36fb7b44c59428de2497b6ebc8
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-12-16 17:03:36 +0530

    Create FourSum.java

---
+++ Arrays/FourSum.java
@@ -0,0 +1,8 @@
+//ide: https://leetcode.com/problems/4sum/
+//sol:https://www.youtube.com/watch?v=4ggF3tXIAp0&list=PLgUwDviBIf0p4ozDR_kJJkONnb1wdx2Ma&index=23
+//https://takeuforward.org/data-structure/4-sum-find-quads-that-add-up-to-a-target-value/
+
+// Time Complexity: O(n³) 
+// Reason: There are 2 nested loops and the front pointer as well as the right pointer (Third nested loop)
+
+// Space Complexity: O(1), (Generally the space complexity that is used to return the answer is ignored)
