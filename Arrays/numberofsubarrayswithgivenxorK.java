3e69645791e42c209d1b70f0da7f91cfc0dbbcc1
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-12-19 03:19:13 +0530

    Update numberofsubarrayswithgivenxorK.java

--- Arrays/numberofsubarrayswithgivenxorK.java
+++ Arrays/numberofsubarrayswithgivenxorK.java
@@ -1,1 +1,29 @@
+//tc:o(nlogn)
+//sc:o(n)
+
+//sol:https://www.youtube.com/watch?v=lO9R5CaGRPY&list=PLgUwDviBIf0p4ozDR_kJJkONnb1wdx2Ma&index=27
+//https://takeuforward.org/data-structure/count-the-number-of-subarrays-with-given-xor-k/
+
+
+//ide:https://www.interviewbit.com/problems/subarray-with-given-xor/
+
+
+HashMap<Integer,Integer> visited = new HashMap<Integer,Integer>(); 
+        int c = 0; 
+        int cpx = 0;
+        int n = A.length;
+        for(int i = 0;i<n;i++) {
+            cpx = cpx ^ A[i]; 
+            if(visited.get(cpx^B) != null) 
+                c += visited.get(cpx ^ B); 
+            if(cpx == B) {
+                c++; 
+            }
+            if(visited.get(cpx) != null) 
+                visited.put(cpx, visited.get(cpx) + 1); 
+            else 
+                visited.put(cpx, 1); 
+        }
+        return c; 
+
 
