7407236f763d2402dc0b60d19a0230e9096feeb5
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-12-20 19:59:18 +0530

    Update PowerSet.java

--- Trie/PowerSet.java
+++ Trie/PowerSet.java
@@ -1,4 +1,4 @@
-
+//theroy for left shift =2^i :https://www.geeksforgeeks.org/left-shift-right-shift-operators-c-cpp/
 //ide: https://practice.geeksforgeeks.org/problems/power-set4302/1#
 //sol:https://www.youtube.com/watch?v=b7AYbpM5YrE&list=PLgUwDviBIf0p4ozDR_kJJkONnb1wdx2Ma&index=69
 //https://takeuforward.org/data-structure/power-set-print-all-the-possible-subsequences-of-the-string/
