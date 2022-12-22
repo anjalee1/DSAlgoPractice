48018e7cf7decc00c8a2d6aebb58e9fc1223ee0b
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-12-22 22:38:11 +0530

    Create MinimumPlatforms.java

---
+++ Greedy/MinimumPlatforms.java
@@ -0,0 +1,6 @@
+//ide: https://practice.geeksforgeeks.org/problems/minimum-platforms-1587115620/1#
+//sol: https://www.youtube.com/watch?v=dxVcMDI7vyI&list=PLgUwDviBIf0p4ozDR_kJJkONnb1wdx2Ma&index=47
+//https://takeuforward.org/data-structure/minimum-number-of-platforms-required-for-a-railway/
+
+//Time Complexity: O(nlogn) Sorting takes O(nlogn) and traversal of arrays takes O(n) so overall time complexity is O(nlogn).
+//Space complexity: O(1) 
