c9447015d1fa267235e74d1a3e6c7d83876b4668
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-12-25 01:27:57 +0530

    Update SubsetSumI.java

--- recursion and backtracking/Recursion_Java/SubsetSumI.java
+++ recursion and backtracking/Recursion_Java/SubsetSumI.java
@@ -1,1 +1,67 @@
 
+//Time Complexity: O(2^n)+O(2^n log(2^n)). Each index has two ways. You can either pick it up or not pick it. So for n index time complexity for O(2^n) and for sorting it will take (2^n log(2^n)).
+//Space Complexity: O(2^n) for storing subset sums, since 2^n subsets can be generated for an array of size n.
+
+
+//IDE: https://practice.geeksforgeeks.org/problems/subset-sums2234/1
+
+//SOL: https://www.youtube.com/watch?v=rYkfBRtMJr8&list=PLgUwDviBIf0p4ozDR_kJJkONnb1wdx2Ma&index=53
+//https://takeuforward.org/data-structure/subset-sum-sum-of-all-subsets/
+
+
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0)
+        {
+            int N=sc.nextInt();
+			ArrayList<Integer> arr = new ArrayList<>();
+			for(int i = 0; i < N ; i++){
+			    arr.add(sc.nextInt());
+			}
+            Solution ob = new Solution();
+         
+            ArrayList<Integer> ans = ob.subsetSums(arr,N);
+            Collections.sort(ans);
+            for(int sum : ans){
+                System.out.print(sum+" ");
+            }
+            System.out.println();
+        }  
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java//User function Template for Java
+class Solution{
+    ArrayList<Integer> subsetSums(ArrayList<Integer> arr, int N){
+        ArrayList < Integer > sumSubset = new ArrayList < > ();
+        subsetSumsHelper(0, 0, arr, N, sumSubset);
+        Collections.sort(sumSubset);
+        return sumSubset;
+    }
+    
+    
+     void subsetSumsHelper(int ind, int sum, ArrayList < Integer > arr, int N, ArrayList < Integer > sumSubset) {
+        if (ind == N) {
+            sumSubset.add(sum);
+            return;
+        }
+
+        // pick the element 
+        subsetSumsHelper(ind + 1, sum + arr.get(ind), arr, N, sumSubset);
+
+        // Do-not pick the element
+        subsetSumsHelper(ind + 1, sum, arr, N, sumSubset);
+    }
+}
