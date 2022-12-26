2adb95f465e6c9443d1fae772c71aed88e13b352
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-12-26 23:26:09 +0530

    Update SubsetSumII.java

--- recursion and backtracking/Recursion_Java/SubsetSumII.java
+++ recursion and backtracking/Recursion_Java/SubsetSumII.java
@@ -1,1 +1,31 @@
 
+//ide:https://leetcode.com/problems/subsets-ii/description/
+//sol: https://www.youtube.com/watch?v=RIn3gOkbhQE&list=PLgUwDviBIf0p4ozDR_kJJkONnb1wdx2Ma&index=54
+//https://takeuforward.org/data-structure/subset-ii-print-all-the-unique-subsets/
+
+//Time Complexity: O(2^n) for generating every subset and O(k)  to insert every subset in another data structure if the average length of every subset is k. Overall O(k * 2^n).
+//Space Complexity: O(2^n * k) to store every subset of average length k. Auxiliary space is O(n)  if n is the depth of the recursion tree.
+
+
+
+
+class Solution {
+
+   public void findSubsets(int ind, int[] nums, List<Integer> ds, List<List<Integer>> ansList) {
+        ansList.add(new ArrayList<>(ds)); 
+        for(int i = ind;i<nums.length;i++) {
+            if(i!=ind && nums[i] == nums[i-1]) continue; 
+            ds.add(nums[i]); 
+            findSubsets(i+1, nums, ds, ansList); 
+            ds.remove(ds.size() - 1);
+        }
+        
+    }
+
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums); 
+        List<List<Integer>> ansList = new ArrayList<>(); 
+        findSubsets(0, nums, new ArrayList<>(), ansList); 
+        return ansList; 
+    }
+}
