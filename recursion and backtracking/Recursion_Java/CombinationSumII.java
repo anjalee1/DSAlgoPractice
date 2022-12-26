71fb50a1d35da72af3aeca2917e4d5a63dcb5a76
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-12-27 02:26:33 +0530

    Update CombinationSumII.java

--- recursion and backtracking/Recursion_Java/CombinationSumII.java
+++ recursion and backtracking/Recursion_Java/CombinationSumII.java
@@ -1,1 +1,34 @@
 
+//ide:https://leetcode.com/problems/combination-sum-ii/description/
+//sol: https://www.youtube.com/watch?v=G1fRTGRxXU8&list=PLgUwDviBIf0p4ozDR_kJJkONnb1wdx2Ma&index=51
+//https://takeuforward.org/data-structure/combination-sum-ii-find-all-unique-combinations/
+
+//Time Complexity:O(2^n*k)
+//Space Complexity:O(k*x)
+
+
+class Solution {
+
+    static void findCombinations(int ind, int[] arr, int target, List < List < Integer >> ans, List < Integer > ds) {
+        if (target == 0) {
+            ans.add(new ArrayList < > (ds));
+            return;
+        }
+
+        for (int i = ind; i < arr.length; i++) {
+            if (i > ind && arr[i] == arr[i - 1]) continue;
+            if (arr[i] > target) break;
+
+            ds.add(arr[i]);
+            findCombinations(i + 1, arr, target - arr[i], ans, ds);
+            ds.remove(ds.size() - 1);
+        }
+    }
+
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List < List < Integer >> ans = new ArrayList < > ();
+        Arrays.sort(candidates);
+        findCombinations(0, candidates, target, ans, new ArrayList < > ());
+        return ans;
+    }
+}
