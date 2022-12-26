af2cb937d7aba22a757b37e58db5f14772c249b2
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-12-27 00:45:07 +0530

    Create CombinationSumI.java

---
+++ recursion and backtracking/Recursion_Java/CombinationSumI.java
@@ -0,0 +1,32 @@
+//ide: https://leetcode.com/problems/combination-sum/description/
+//sol: https://www.youtube.com/watch?v=OyZFFqQtu98&list=PLgUwDviBIf0p4ozDR_kJJkONnb1wdx2Ma&index=50
+//https://takeuforward.org/data-structure/combination-sum-1/
+
+//Time Complexity: O(2^t * k) where t is the target, k is the average length
+//Space Complexity: O(k*x), k is the average length and x is the no. of combinations
+
+
+
+class Solution {
+    private void findCombinations(int ind, int[] arr, int target, List < List < Integer >> ans, List < Integer > ds) {
+        if (ind == arr.length) {
+            if (target == 0) {
+                ans.add(new ArrayList < > (ds));
+            }
+            return;
+        }
+
+        if (arr[ind] <= target) {
+            ds.add(arr[ind]);
+            findCombinations(ind, arr, target - arr[ind], ans, ds);
+            ds.remove(ds.size() - 1);
+        }
+        findCombinations(ind + 1, arr, target, ans, ds);
+    }
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List < List < Integer >> ans = new ArrayList < > ();
+        findCombinations(0, candidates, target, ans, new ArrayList < > ());
+        return ans;
+    }
+}
