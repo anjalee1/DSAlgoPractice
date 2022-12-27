eadf30f7264cf430b9f8baae28614e4cb3e101a8
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-12-27 16:33:08 +0530

    Update K-thPermutationSequence.java

--- recursion and backtracking/Recursion_Java/K-thPermutationSequence.java
+++ recursion and backtracking/Recursion_Java/K-thPermutationSequence.java
@@ -4,3 +4,26 @@
 
 //TimeComplexity: O(N) * O(N) = O(N^2)
 //SpaceComplexity: O(N) 
+
+
+
+int fact = 1;
+        ArrayList < Integer > numbers = new ArrayList < > ();
+        for (int i = 1; i < n; i++) {
+            fact = fact * i;
+            numbers.add(i);
+        }
+        numbers.add(n);
+        String ans = "";
+        k = k - 1;
+        while (true) {
+            ans = ans + "" + numbers.get(k / fact);
+            numbers.remove(k / fact);
+            if (numbers.size() == 0) {
+                break;
+            }
+
+            k = k % fact;
+            fact = fact / numbers.size();
+        }
+        return ans;
