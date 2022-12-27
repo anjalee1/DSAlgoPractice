0d9a6fe776bc0294d698296e2209f988f9fea081
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-12-27 17:56:48 +0530

    Update PrintallPermutationsofaStringArray.java

--- recursion and backtracking/Recursion_Java/PrintallPermutationsofaStringArray.java
+++ recursion and backtracking/Recursion_Java/PrintallPermutationsofaStringArray.java
@@ -1,1 +1,41 @@
 
+//ide: https://leetcode.com/problems/permutations/
+//sol: https://www.youtube.com/watch?v=f2ic2Rsc9pU&list=PLgUwDviBIf0p4ozDR_kJJkONnb1wdx2Ma&index=53
+//https://takeuforward.org/data-structure/print-all-permutations-of-a-string-array/
+
+
+//Time Complexity: O(N! X N)
+//Space Complexity: O(1)
+
+
+class Solution {
+
+     private void recurPermute(int index, int[] nums, List < List < Integer >> ans) {
+        if (index == nums.length) {
+            // copy the ds to ans
+            List < Integer > ds = new ArrayList < > ();
+            for (int i = 0; i < nums.length; i++) {
+                ds.add(nums[i]);
+            }
+            ans.add(new ArrayList < > (ds));
+            return;
+        }
+        for (int i = index; i < nums.length; i++) {
+            swap(i, index, nums);
+            recurPermute(index + 1, nums, ans);
+            swap(i, index, nums);
+        }
+    }
+    
+    private void swap(int i, int j, int[] nums) {
+        int t = nums[i];
+        nums[i] = nums[j];
+        nums[j] = t;
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        List < List < Integer >> ans = new ArrayList < > ();
+        recurPermute(0, nums, ans);
+        return ans;
+    }
+}
