d80b11a82c00094ed302a83a02a2a5e1ac4ce730
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2022-12-27 16:31:44 +0530

    Create K-thPermutationSequence.java

---
+++ recursion and backtracking/Recursion_Java/K-thPermutationSequence.java
@@ -0,0 +1,6 @@
+//ide: https://leetcode.com/problems/permutation-sequence/
+//sol: https://www.youtube.com/watch?v=wT7gcXLYoao&list=PLgUwDviBIf0p4ozDR_kJJkONnb1wdx2Ma&index=56
+//https://takeuforward.org/data-structure/find-k-th-permutation-sequence/
+
+//TimeComplexity: O(N) * O(N) = O(N^2)
+//SpaceComplexity: O(N) 
