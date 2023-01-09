92864572e1a0043cdd66973787c739cab3bef3ba
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2023-01-10 03:11:12 +0530

    Update 328_OddEvenLinkedList.java

--- LeetCodeTopInterviewQue/LinkedList/328_OddEvenLinkedList.java
+++ LeetCodeTopInterviewQue/LinkedList/328_OddEvenLinkedList.java
@@ -5,3 +5,22 @@
 //sc:o(1)
 
 
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if(head==null) return head;
+        ListNode odd = head;
+        ListNode even =head.next;
+
+        ListNode odd_head=head;
+        ListNode even_head=head.next;
+
+        while(even != null && even.next != null){
+            odd.next =odd.next.next;
+            even.next=even.next.next;
+            odd=odd.next;
+            even=even.next;
+        }
+        odd.next=even_head;
+        return head;
+    }
+}
