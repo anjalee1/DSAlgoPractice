c51c7efe5323278ce6478e020e72e49a35fd5b42
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2023-01-09 18:55:18 +0530

    Create 328_OddEvenLinkedList.java

---
+++ LeetCodeTopInterviewQue/LinkedList/328_OddEvenLinkedList.java
@@ -0,0 +1,7 @@
+//ide: https://leetcode.com/problems/odd-even-linked-list/
+//sol: q:https://www.youtube.com/watch?time_continue=4&v=58o1--_XGC4
+//ans: https://www.youtube.com/watch?time_continue=2&v=calrG6RJ9qI
+//https://nados.io/question/odd-even-linked-list
+
+//tc:
+//sc:
