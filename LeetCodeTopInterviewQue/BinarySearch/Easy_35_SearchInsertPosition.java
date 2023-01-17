ae8844d278406ef510c9cee530647d69d67616d0
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2023-01-17 21:51:34 +0530

    Update Easy_35_SearchInsertPosition.java

--- LeetCodeTopInterviewQue/BinarySearch/Easy_35_SearchInsertPosition.java
+++ LeetCodeTopInterviewQue/BinarySearch/Easy_35_SearchInsertPosition.java
@@ -1,1 +1,26 @@
+//ide: https://leetcode.com/problems/search-insert-position/
+//tc: log(n)  sc: o(1)
 
+//same logic used in upper bound function
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int lo= 0;
+        int hi =nums.length-1;
+
+        while(lo<=hi){
+            int mid = lo+(hi-lo)/2;
+
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                lo=mid+1;
+            }
+            else{
+                hi=mid-1;
+            }
+        }
+        return lo;
+    }
+}
