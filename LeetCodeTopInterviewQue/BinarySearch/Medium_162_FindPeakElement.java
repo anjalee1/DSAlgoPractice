e9f976e472fa81f44097021e3405a4693b4080f5
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2023-01-19 01:50:14 +0530

    Update Medium_162_FindPeakElement.java

--- LeetCodeTopInterviewQue/BinarySearch/Medium_162_FindPeakElement.java
+++ LeetCodeTopInterviewQue/BinarySearch/Medium_162_FindPeakElement.java
@@ -1,1 +1,27 @@
 
+//ide : https://leetcode.com/problems/find-peak-element/description/
+//tc: o(logn)  sc:o(1)
+//sol: https://leetcode.com/problems/find-peak-element/solutions/1290642/intuition-behind-conditions-complete-explanation-diagram-binary-search/
+class Solution {
+    public int findPeakElement(int[] nums) {
+     if(nums.length == 1) return 0; // single element
+        
+        int n = nums.length;
+        
+		// check if 0th/n-1th index is the peak element
+        if(nums[0] > nums[1]) return 0;
+        if(nums[n-1] > nums[n-2]) return n-1;
+		
+		// search in the remaining array
+        int start = 1;
+        int end = n-2;
+        
+        while(start <= end) {
+            int mid = start + (end - start)/2;
+            if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]) return mid;
+            else if(nums[mid] < nums[mid-1]) end = mid - 1;
+            else if(nums[mid] < nums[mid+1]) start = mid + 1;
+        }
+        return -1; // dummy return statement   
+    }
+}
