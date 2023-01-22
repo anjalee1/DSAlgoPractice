bf1f0364ff29bf135f27fa948117cc3eb1ea0824
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2023-01-22 17:39:25 +0530

    Update SortAStack.java

--- Stack/Stack-Java/SortAStack.java
+++ Stack/Stack-Java/SortAStack.java
@@ -4,7 +4,7 @@
 //sc=o(n)
   
  //ide: https://www.codingninjas.com/codestudio/problems/sort-a-stack_985275?leftPanelTab=1
-  //video- second q :https://www.youtube.com/watch?v=BmZnJehDzyU&t=3017
+  //video- 6th que :https://www.youtube.com/watch?v=BmZnJehDzyU&t=3017
  // tc expanation: https://www.enjoyalgorithms.com/blog/sort-stack-using-temporary-stack
 import java.util.*;
 
