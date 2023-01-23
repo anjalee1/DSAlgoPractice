462858c45cfd651a0f963f0547f3f5643f17f3ea
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2023-01-24 01:19:52 +0530

    Update CelebrityProblem.java

--- Stack/Stack-Java/CelebrityProblem.java
+++ Stack/Stack-Java/CelebrityProblem.java
@@ -1,38 +1,71 @@
+//{ Driver Code Starts
+//Initial Template for Java
 
-import java.util.* ;
-import java.io.*; 
+import java.io.*;
+import java.util.*; 
 
-public class Solution {
-	public static int findCelebrity(int n) {
-        Stack<Integer> st= new Stack<Integer>();
-        for(int i=0;i<n;i++){
-            st.push(i);
-        } 
-        while(st.size()>1){
-            int i=st.pop();
-            int j=st.pop();
-            if(Runner.knows(i,j)){
-                st.push(j);
-            }
-            else{
-                st.push(i);
-            }
-        } 
-        if(st.empty())
-            return -1;
-        int pot=st.pop();
-        for(int i=0;i<n;i++){
-            if(i!=pot){
-                if(Runner.knows(i,pot)==false || Runner.knows(pot,i)==true){
-                    return -1;
+class GFG{
+    public static void main(String args[]) throws IOException { 
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t>0)
+        {
+            int N = sc.nextInt();
+            int M[][] = new int[N][N];
+            for(int i=0; i<N; i++)
+            {
+                for(int j=0; j<N; j++)
+                {
+                    M[i][j] = sc.nextInt();
                 }
             }
-         }
-        return pot; 
+            System.out.println(new Solution().celebrity(M,N));
+            t--;
+        }
+    } 
+} 
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution
+{ 
+    //Function to find if there is a celebrity in the party or not.
+    int celebrity(int M[][], int n)
+    {
+        Stack<Integer> st = new Stack<>();
+    	for(int i=0;i<n;i++){
+    	    st.push(i);
+    	}
+    	
+    	while(st.size()>=2){
+    	    int i=st.pop();
+    	    int j=st.pop();
+    	    
+    	    if(M[i][j]==1){
+    	        st.push(j);
+    	    }
+    	    else{
+    	        st.push(i);
+    	    }
+    	}
+    	
+    	int pot =st.pop();
+    	
+    	for(int i=0;i<n;i++){
+    	    if(i!=pot){
+    	        if(M[i][pot]==0 || M[pot][i]==1){
+    	            return -1;
+    	        }
+    	    }
+    	}
+    	
+    	return pot;
     }
 }
-
-//solution video:nsoluhttps://www.youtube.com/watch?v=CiiXBvrX-5A&t=7s
+//solution:https://www.youtube.com/watch?v=CiiXBvrX-5A&t=7s
 //ide: https://practice.geeksforgeeks.org/problems/the-celebrity-problem/1
 //tc:o(N)
 //sc:o(N)
