31f4f86aa4104643e05a5523d9b10ef365221881
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2023-02-23 22:03:37 +0530

    Update MinimumCostInMaze.java

--- DyanamicProgramming_Inter/Pep_DP_JAVA/MinimumCostInMaze.java
+++ DyanamicProgramming_Inter/Pep_DP_JAVA/MinimumCostInMaze.java
@@ -3,3 +3,32 @@
 
 //tc:o(n^2),sc:o(n^2)
 
+class Solution {
+    public int minPathSum(int[][] grid) {
+        
+        int r =grid.length;
+        int c= grid[0].length;
+
+        int dp[][]=new int[r][c];
+        for(int i=r-1;i>=0;i--){
+            for(int j=c-1;j>=0;j--){
+                if(i==r-1 && j==c-1){
+                   dp[i][j]=grid[i][j];
+                }
+                else if(i==r-1){
+                  dp[i][j]=dp[i][j+1]+grid[i][j];
+                }
+                else if(j==c-1){
+                    dp[i][j]=dp[i+1][j]+grid[i][j];
+                }
+                else{
+                   dp[i][j]=Math.min(dp[i+1][j],dp[i][j+1])+grid[i][j];
+                }
+            }
+        }
+
+        return dp[0][0];
+        
+    }
+}
+
