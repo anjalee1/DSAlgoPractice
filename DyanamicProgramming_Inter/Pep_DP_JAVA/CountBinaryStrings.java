a1596b2debb2de9ba5ea465fbdf99b573e6ae32f
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2023-03-03 14:25:41 +0530

    Update CountBinaryStrings.java

--- DyanamicProgramming_Inter/Pep_DP_JAVA/CountBinaryStrings.java
+++ DyanamicProgramming_Inter/Pep_DP_JAVA/CountBinaryStrings.java
@@ -1,5 +1,32 @@
-//ide: 
-//sol:
+//ide: https://nados.io/question/count-binary-strings
+//sol:https://www.youtube.com/watch?v=nqrXHJWMeBc&t=3s
 
-//tc:
-//sc: 
+//tc:o(n)
+//sc: o(n)
+
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+public static void main(String[] args) throws Exception {
+   Scanner scn = new Scanner(System.in);
+   int n = scn.nextInt();
+
+   int oczeroes= 1;
+   int ocones=1;
+
+   for(int i=2;i<=n;i++){
+       int nczeros=ocones;
+       int ncones=oczeroes+ocones;
+
+       oczeroes=nczeros;
+       ocones=ncones;
+
+   }
+
+   System.out.println(oczeroes+ocones);
+   
+ }
+
+}
