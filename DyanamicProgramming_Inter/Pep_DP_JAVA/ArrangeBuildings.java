e316633c3f2fd6887e7d60138e6e12771dd713b9
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2023-03-03 16:03:27 +0530

    Create ArrangeBuildings.java

---
+++ DyanamicProgramming_Inter/Pep_DP_JAVA/ArrangeBuildings.java
@@ -0,0 +1,34 @@
+//ide:https://nados.io/question/arrange-buildings
+//sol: https://www.youtube.com/watch?time_continue=1&v=0nF-BMYy7tc
+
+//tc:o(n
+//sc:o(n)
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+   public static void main(String[] args) throws Exception {
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      int n = Integer.parseInt(br.readLine());
+
+      long zeroes = 1;
+      long ones = 1;
+      for (int i = 2; i <= n; i++) {
+         long nzeroes = ones;
+         long nones = ones + zeroes;
+
+         zeroes = nzeroes;
+         ones = nones;
+      }
+
+      long oneside = zeroes + ones;
+      System.out.println(oneside * oneside);
+   }
+
+}
+
+
+                        
