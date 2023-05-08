98402cb24a9e35ecf86c09647004c9d43ffc85c9
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2023-05-09 02:27:57 +0530

    Update NumberOfIslands.java

--- Graphs/Graph_Java/NumberOfIslands.java
+++ Graphs/Graph_Java/NumberOfIslands.java
@@ -70,5 +70,46 @@ class Solution {
 //tc: n^2
 //sc: n^2
 
+
+
+class Solution {
+    
+    private void dfs(int row ,int col, int [][] vis,char[][] grid){
+        vis[row][col]=1;
+        int m =grid.length;
+        int n =grid[0].length;
+        for(int delRow =-1 ;delRow<=1;delRow++){
+           for(int delCol=-1 ;delCol<=1;delCol++){
+               if((delRow==-1 && delCol==-1) || (delRow==-1 && delCol==1) || 
+                  (delRow==1 && delCol==-1)  || (delRow==1  && delCol==1)){
+                    continue;
+                }
+               int nRow= row+delRow;
+               int nCol= col+delCol;
+               if(nRow>=0 && nRow<m && nCol>=0 && nCol<n
+                 && grid[nRow][nCol]=='1' &&  vis[nRow][nCol]==0){
+                   dfs(nRow,nCol,vis,grid);
+               }   
+            }
+         }
+    }
+    
+    public int numIslands(char[][] grid) {
+        int m=grid.length;
+        int n=grid[0].length;
+        int [][] vis=new int[m][n];
+        int cnt =0;
+        for(int row =0 ;row<m;row++){
+            for(int col=0 ;col<n;col++){
+                if(vis[row][col]==0 && grid[row][col]=='1'){
+                    cnt++;
+                    dfs(row,col,vis,grid);
+                }
+            }   
+        }
+        return cnt;
+    }
+}
+
 //ide:https://leetcode.com/problems/number-of-islands/
 //sol: https://www.youtube.com/watch?v=muncqlKJrH0&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&index=9
