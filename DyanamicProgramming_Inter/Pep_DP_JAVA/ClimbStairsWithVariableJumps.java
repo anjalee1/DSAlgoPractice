83a14ad1be3d133864504e6c2c5dbec5dff29f8c
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2023-06-05 01:04:16 +0530

    Update ClimbStairsWithVariableJumps.java

--- DyanamicProgramming_Inter/Pep_DP_JAVA/ClimbStairsWithVariableJumps.java
+++ DyanamicProgramming_Inter/Pep_DP_JAVA/ClimbStairsWithVariableJumps.java
@@ -2,31 +2,33 @@
 //ide: https://nados.io/question/climb-stairs-with-variable-jumps
 //https://www.youtube.com/watch?v=uNqoQ0sNZCM&t=697s
 
-import java.io.*;
-import java.util.*;
-
-public class Main {
-
-    public static void main(String[] args) throws Exception {
-       Scanner scn = new Scanner(System.in);
-       int n= scn.nextInt();
-
-       int arr[] = new int[n];
-
-       for(int i =0; i<n;i++){
-           arr[i] =scn.nextInt();
-       }
-       int dp[] = new int[n+1];
-       dp[n]=1;
-
-       for(int i=n-1;i>=0;i--){
-           for(int j=1;j<=arr[i]&& i+j<=n;j++){
-
-               dp[i]+=dp[i+j];
-
+    import java.io.*;
+    import java.util.*;
+     
+    public class Main {
+     
+        public static void main(String[] args) throws Exception {
+           Scanner scn = new Scanner(System.in);
+           int n= scn.nextInt();
+     
+           int arr[] = new int[n];
+     
+           for(int i =0; i<n;i++){
+               arr[i] =scn.nextInt();
            }
-       }
-       System.out.println(dp[0]);
+           int dp[] = new int[n+1];
+     
+           for(int i=n-1;i>=0;i--){
+               for(int j=1;j<=arr[i]&& i+j<=n;j++){
+                     if(i+j==n){
+                     	 dp[i]+=1;
+                     }else{
+                     	 dp[i]+=dp[i+j];
+                     }
+     
+               }
+           }
+           System.out.println(dp[0]);
+        }
+     
     }
-
-}
