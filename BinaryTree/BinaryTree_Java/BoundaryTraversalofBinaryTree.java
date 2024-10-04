c6a698e19749776989242be249d582f532e85249
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2024-10-04 18:08:52 +0530

    Update BoundaryTraversalofBinaryTree.java

--- BinaryTree/BinaryTree_Java/BoundaryTraversalofBinaryTree.java
+++ BinaryTree/BinaryTree_Java/BoundaryTraversalofBinaryTree.java
@@ -30,49 +30,59 @@ import java.io.*;
 
 import java.util.ArrayList;
 
-public class Solution {
-    
-    public static Boolean isLeaf(TreeNode root) {
-        return (root.left == null) && (root.right == null);
-    }
-    
-    public static void addLeftBoundary(TreeNode root, ArrayList < Integer > res) {
-        TreeNode cur = root.left;
-        while (cur != null) {
-            if (isLeaf(cur) == false) res.add(cur.data);
-            if (cur.left != null) cur = cur.left;
-            else cur = cur.right;
+class Solution {
+    ArrayList <Integer> boundary(Node node) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        if (!isLeaf(node)) ans.add(node.data);
+        
+        // Add the left boundary
+        Node curr = node.left;
+        while (curr != null) {
+            if (!isLeaf(curr)) ans.add(curr.data);
+            if (curr.left != null) curr = curr.left;
+            else curr = curr.right;
         }
-    }
-    
-   public static void addRightBoundary(TreeNode root, ArrayList < Integer > res) {
-        TreeNode cur = root.right;
-        ArrayList < Integer > tmp = new ArrayList < Integer > ();
-        while (cur != null) {
-            if (isLeaf(cur) == false) tmp.add(cur.data);
-            if (cur.right != null) cur = cur.right;
-            else cur = cur.left;
+        
+        // Add the leaf nodes
+        addLeaves(node, ans);
+        
+        // Add the right boundary in reverse
+        curr = node.right;
+        ArrayList<Integer> temp = new ArrayList<>();
+        while (curr != null) {
+            if (!isLeaf(curr)) temp.add(curr.data);
+            if (curr.right != null) curr = curr.right;
+            else curr = curr.left;
         }
-        int i;
-        for (i = tmp.size() - 1; i >= 0; --i) {
-            res.add(tmp.get(i));
+        
+        // Reverse the right boundary before adding
+        for (int i = temp.size() - 1; i >= 0; i--) {
+            ans.add(temp.get(i));
         }
-    }
-    
-     public static void addLeaves(TreeNode root, ArrayList < Integer > res) {
-        if (isLeaf(root)) {
-            res.add(root.data);
-            return;
-        }
-        if (root.left != null) addLeaves(root.left, res);
-        if (root.right != null) addLeaves(root.right, res);
-    }
-	public static ArrayList<Integer> traverseBoundary(TreeNode root){
-		ArrayList < Integer > ans = new ArrayList < Integer > ();
-        if (isLeaf(root) == false) ans.add(root.data);
-        addLeftBoundary(root, ans);
-        addLeaves(root, ans);
-        addRightBoundary(root, ans);
+        
         return ans;
-	}
+    }
+
+    boolean isLeaf(Node node) {
+        return (node.left == null) && (node.right == null);
+    }
+
+    void addLeaves(Node root, List<Integer> ans) {
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+        
+        while (!stack.isEmpty()) {
+            Node curr = stack.pop();
+            
+            if (isLeaf(curr)) {
+                ans.add(curr.data);
+            } else {
+                // Push right child first so that left is processed first
+                if (curr.right != null) stack.push(curr.right);
+                if (curr.left != null) stack.push(curr.left);
+            }
+        }
+    }
 }
+
+
