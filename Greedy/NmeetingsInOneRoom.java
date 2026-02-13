8446a000f79172c0f856eb8a4195f2fc24b92c45
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2026-02-14 01:59:51 +0530

    Update NmeetingsInOneRoom.java

--- Greedy/NmeetingsInOneRoom.java
+++ Greedy/NmeetingsInOneRoom.java
@@ -14,84 +14,26 @@ import java.io.*;
 import java.util.*;
 import java.lang.*;
 
-class GFG {
-    public static void main(String args[]) throws IOException {
-        BufferedReader br =
-            new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine().trim());
-
-        while (t-- > 0) {
-            String inputLine[] = br.readLine().trim().split(" ");
-            int n = Integer.parseInt(inputLine[0]);
-
-            int start[] = new int[n];
-            int end[] = new int[n];
-
-            inputLine = br.readLine().trim().split(" ");
-            for (int i = 0; i < n; i++)
-                start[i] = Integer.parseInt(inputLine[i]);
-
-            inputLine = br.readLine().trim().split(" ");
-            for (int i = 0; i < n; i++) 
-                end[i] = Integer.parseInt(inputLine[i]);
-                
-            int ans = new Solution().maxMeetings(start, end, n);
-            System.out.println(ans);
+class Solution {
+    // Function to find the maximum number of meetings that can
+    // be performed in a meeting room.
+    public int maxMeetings(int start[], int end[]) {
+        
+        List<List<Integer>> timings= new ArrayList<>();
+        for(int i=0;i<start.length;i++){
+            timings.add(List.of(start[i],end[i]));
         }
-    }
-}
-
-// } Driver Code Ends
-
-class meeting {
-    int start;
-    int end;
-    int pos;
-     
-    meeting(int start, int end, int pos)
-    {
-        this.start = start;
-        this.end = end;
-        this.pos = pos;
-    }
-}
-class meetingComparator implements Comparator<meeting>
-{
-    @Override
-    public int compare(meeting o1, meeting o2) 
-    {
-        if (o1.end < o2.end)
-            return -1;
-        else if (o1.end > o2.end)
-            return 1;
-        else if(o1.pos < o2.pos)
-            return -1;
-        return 1; 
-    }
-}
-class Solution 
-{
-    //Function to find the maximum number of meetings that can
-    //be performed in a meeting room.
-    public static int maxMeetings(int start[], int end[], int n)
-    {
-        ArrayList<meeting> meet = new ArrayList<>();
-        
-        for(int i = 0; i < start.length; i++)
-            meet.add(new meeting(start[i], end[i], i+1));
-        
-        meetingComparator mc = new meetingComparator(); 
-        Collections.sort(meet, mc); 
-        int meetingCount=1;
-        int limit = meet.get(0).end; 
-        
-        for(int i = 1;i<start.length;i++) {
-            if(meet.get(i).start > limit) {
-                limit = meet.get(i).end; 
-                meetingCount++;
+        int count= 1;
+        Collections.sort(timings, (a,b)->a.get(1)-b.get(1));
+        int limit = timings.get(0).get(1);
+        for(int i=1;i<timings.size();i++){
+            if(timings.get(i).get(0)>limit){
+                limit=timings.get(i).get(1);
+                count++;
             }
+            
         }
         
-        return meetingCount;
+        return count;
     }
 }
