e1ac46d5433bb5b7570aa71e42930afb14974d1a
Anjali Upadhyay <49253598+anjalee1@users.noreply.github.com>
2026-02-21 20:05:08 +0530

    Update SingleElementinaSortedArray.java

--- BinarySearch/BinarySearch_Java/SingleElementinaSortedArray.java
+++ BinarySearch/BinarySearch_Java/SingleElementinaSortedArray.java
@@ -66,3 +66,35 @@ class Solution {
         
     }
 }
+
+
+
+
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int n = nums.length;
+        if (n == 1) return nums[0];
+        if (nums[0] != nums[1]) return nums[0];
+        if (nums[n - 1] != nums[n - 2]) return nums[n - 1];
+
+        int lo = 1, hi = n - 2;
+        
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+
+            // Found the single element
+            if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1]) {
+                return nums[mid];
+            }
+
+            // Check if we are in the left half (pattern is intact)
+            if ((mid % 2 == 1 && nums[mid] == nums[mid - 1]) || 
+                (mid % 2 == 0 && nums[mid] == nums[mid + 1])) {
+                lo = mid + 1; // Eliminate left half
+            } else {
+                hi = mid - 1; // Eliminate right half
+            }
+        }
+        return -1;
+    }
+}
